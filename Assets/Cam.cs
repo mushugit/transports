f8f27d6adb91afbb7cb075e9b8aac7e6da5d47a9
mushugit <marmouchon@gmail.com>
2018-06-05 23:50:14 +0200

    Première version

---
+++ Assets/Cam.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cam : MonoBehaviour {
+
+	public float cameraMoveSpeed = 30f;
+	public float cameraZoomSpeed = 100f;
+
+	Vector3 defaultPosition;
+
+	// Use this for initialization
+	void Start () {
+		defaultPosition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.Escape))
+			Application.Quit ();
+
+		var r = transform.rotation;
+		transform.rotation = new Quaternion(0f, 0f, 0f, 0f);
+
+		if(Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.Q))
+			transform.Translate(new Vector3(-cameraMoveSpeed*Time.deltaTime,0,cameraMoveSpeed*Time.deltaTime));
+
+		if(Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+			transform.Translate(new Vector3(cameraMoveSpeed*Time.deltaTime,0,-cameraMoveSpeed*Time.deltaTime));
+
+		if (Input.GetKey (KeyCode.UpArrow) || Input.GetKey (KeyCode.Z))
+			transform.Translate(new Vector3 (cameraMoveSpeed * Time.deltaTime, 0, cameraMoveSpeed * Time.deltaTime));
+
+		if(Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
+			transform.Translate(new Vector3(-cameraMoveSpeed*Time.deltaTime,0,-cameraMoveSpeed*Time.deltaTime));
+
+		if (Input.GetKey (KeyCode.R))
+			transform.position = defaultPosition;
+
+		transform.rotation = r;
+
+		if (Input.GetAxis ("Mouse ScrollWheel") < 0)
+			transform.Translate (new Vector3 (0f, 0f, -cameraZoomSpeed * Time.deltaTime));
+
+		if (Input.GetAxis ("Mouse ScrollWheel") > 0)
+			transform.Translate (new Vector3 (0f, 0f, cameraZoomSpeed * Time.deltaTime));
+	}
+}
---
+++ Assets/World.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class World : MonoBehaviour {
+	public Material grassMaterial;
+	// Use this for initialization
+	void Start () {
+		var cellSize = new Vector3(1F, 0.05F, 1F);
+		const float width = 10F;
+		const float height = 10F;
+
+		Debug.Log (grassMaterial.name);
+
+		for (float x = 0.5F; x < width; x++) {
+			for (float y = 0.5F; y < height; y++) {
+				GameObject cube = GameObject.CreatePrimitive (PrimitiveType.Cube);
+				cube.transform.position = new Vector3 (x,0F,y);
+				cube.transform.localScale = cellSize;
+				cube.GetComponent<Renderer>().material = grassMaterial;
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
