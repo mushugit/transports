9aae493c11cf96ac13856e6963927b18e49b9830
Mushu <Mushu>
2018-06-17 00:31:40 +0200

    Clamping en cercle autour du centre Rotation de la camera Scrolling avec maj

--- Assets/Scripts/Cam.cs
+++ Assets/Scripts/Cam.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 public class Cam : MonoBehaviour
 {
 	Vector3 defaultPosition;
+	Quaternion defaultRotation;
+
+	Vector3 lastValidPosition;
 
 	public GameObject CamReferencePosition;
 
@@ -15,6 +18,11 @@ public class Cam : MonoBehaviour
 	public float edgeScrollSize = 20f;
 	public float scrollSpeed = 5f;
 	public float edgeLimit = 7f;
+
+	public float camRadiusRatio = 0.85f;
+	float camRadius;
+
+	public float rotateSpeed = 25f;
 
 	readonly float defaultCoord = -12f;
 	readonly Vector3 referencePoint = new Vector3(0f, -2f, 0f);
@@ -34,11 +42,12 @@ public class Cam : MonoBehaviour
 		}*/
 
 		CamReferencePosition.transform.position = defaultPosition;
-
+		transform.rotation = defaultRotation;
 	}
 
 	void Start()
 	{
+		camRadius = camRadiusRatio * Mathf.Max(World.width, World.height);
 		defaultPosition = CamReferencePosition.transform.position;
 
 		defaultPosition.x = (World.width / defaultZoomPosition) + defaultCoord;
@@ -50,21 +59,20 @@ public class Cam : MonoBehaviour
 		do
 		{
 			mapReferenceScreenPoint = Camera.main.WorldToScreenPoint(referencePoint);
-			Debug.Log(mapReferenceScreenPoint);
+			//Debug.Log(mapReferenceScreenPoint);
 			transform.Translate(Vector3.back);
 		} while (mapReferenceScreenPoint.y < 0);
 
 
 		defaultPosition = CamReferencePosition.transform.position;
+		defaultRotation = transform.rotation;
 	}
 
 	void Update()
 	{
 		var t = CamReferencePosition.transform;
-		var originalPosition = t.position;
+		lastValidPosition =  t.position;
 		var moveFactor = moveSpeed * Time.deltaTime;
-		/*if (World.gameLoading)
-            return;*/
 
 		// Reset
 		if (Input.GetButtonDown("ResetView"))
@@ -73,35 +81,31 @@ public class Cam : MonoBehaviour
 		}
 
 		// Edge scrolling
-		var mousePosition = Input.mousePosition;
-		var scrollVector = new Vector3();
-		if (mousePosition.x < edgeScrollSize)
-			scrollVector.x = -scrollSpeed * Time.deltaTime;
-		if (mousePosition.x > Screen.width - edgeScrollSize)
-			scrollVector.x = scrollSpeed * Time.deltaTime;
-		if (mousePosition.y < edgeScrollSize)
-			scrollVector.z = -scrollSpeed * Time.deltaTime;
-		if (mousePosition.y > Screen.height - edgeScrollSize)
-			scrollVector.z = scrollSpeed * Time.deltaTime;
+		if (Input.GetButton("Edge scroll"))
+		{
+			var mousePosition = Input.mousePosition;
+			var scrollVector = new Vector3();
+			if (mousePosition.x < edgeScrollSize)
+				scrollVector.x = -scrollSpeed * Time.deltaTime;
+			if (mousePosition.x > Screen.width - edgeScrollSize)
+				scrollVector.x = scrollSpeed * Time.deltaTime;
+			if (mousePosition.y < edgeScrollSize)
+				scrollVector.z = -scrollSpeed * Time.deltaTime;
+			if (mousePosition.y > Screen.height - edgeScrollSize)
+				scrollVector.z = scrollSpeed * Time.deltaTime;
 
-		if (scrollVector != null)
-			t.Translate(scrollVector, t);
-			
-
+			if (scrollVector != null)
+				t.Translate(scrollVector, t);
+		}
 
 		// Directionnal move
 		t.Translate(Input.GetAxis("Horizontal") * moveFactor, 0f, Input.GetAxis("Vertical") * moveFactor);
 
-		
+		// Rotation
+		t.RotateAround(new Vector3(World.width / 2f, 0f, World.height / 2f), Vector3.up, Input.GetAxis("Rotate") * rotateSpeed * Time.deltaTime);
+
 		// Clamp
-		if (t.position.z > t.position.x + World.height)
-			t.position = originalPosition;
-		if (t.position.z < t.position.x - World.width)
-			t.position = originalPosition;
-		if (t.position.z > -(t.position.x - edgeLimit) + World.width)
-			t.position = originalPosition;
-		if (t.position.z < -(t.position.x - edgeLimit) - World.height)
-			t.position = originalPosition;
+		t.position = ClampCircle(lastValidPosition, t.position);
 
 		// Zoom
 		var positionBeforeZoom = t.position;
@@ -109,7 +113,29 @@ public class Cam : MonoBehaviour
 
 		if (transform.position.y < minZoom || transform.position.y > maxZoom)
 			transform.position = positionBeforeZoom;
-			
 
+		lastValidPosition = t.position;
+
+	}
+
+	Vector3 ClampSquare(Vector3 originalPosition, Vector3 position)
+	{
+		if (position.z > position.x + World.height)
+			return originalPosition;
+		if (position.z < position.x - World.width)
+			return originalPosition;
+		if (position.z > -(position.x - edgeLimit) + World.width)
+			return originalPosition;
+		if (position.z < -(position.x - edgeLimit) - World.height)
+			return originalPosition;
+		return position;
+	}
+
+	Vector3 ClampCircle(Vector3 originalPosition, Vector3 position)
+	{
+		//Debug.Log($"Check {position} : {(position.x - World.width / 2f)}²+{(position.z - World.height / 2f)}² > {camRadius}²");
+		if ((position.x - World.width / 2f) * (position.x - World.width / 2f) + (position.z - World.height / 2f) * (position.z - World.height / 2f) > camRadius * camRadius)
+			return originalPosition;
+		return position;
 	}
 }
