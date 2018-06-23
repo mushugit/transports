6e92e7fccb0fb456faaaf90cfbb1b21a294e9d0a
Mushu <Mushu>
2018-06-23 12:14:47 +0200

    TRN-10 Ajout d'un audio manager qui gère les sons et les musiques Ajout de 3 musiques de fond

---
+++ Assets/AudioManager.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+using System;
+
+public class AudioManager : MonoBehaviour
+{
+
+	public Sound[] sounds;
+	private Sound[] music;
+	private int currentMusic = 0;
+
+	private static AudioManager instance;
+
+	private void Awake()
+	{
+		DontDestroyOnLoad(gameObject);
+
+		if (instance == null)
+			instance = this;
+		else
+		{
+			Destroy(gameObject);
+			return;
+		}
+
+		foreach (Sound s in sounds)
+		{
+			s.source = gameObject.AddComponent<AudioSource>();
+
+			s.source.clip = s.clip;
+			s.source.volume = s.volume;
+			s.source.pitch = s.pitch;
+		}
+	}
+
+	private void Start()
+	{
+		music = Array.FindAll(sounds, sound => sound.isMusic);
+		if(music.Length > 0)
+		{
+			var length = music[currentMusic].source.clip.length;
+			Play(music[currentMusic].name);
+			Invoke("NextMusic", length);
+		}
+	}
+
+	private void NextMusic()
+	{
+		currentMusic++;
+		if (currentMusic >= music.Length)
+			currentMusic = 0;
+
+		Play(music[currentMusic].name);
+	}
+
+	public void Play(string name)
+	{
+		var s = Array.Find(sounds, sound => sound.name == name);
+
+		s?.source.Play();
+	}
+}
---
+++ Assets/Sound.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+
+[System.Serializable]
+public class Sound {
+
+	public string name;
+
+	public AudioClip clip;
+
+	public bool isMusic = false;
+
+	[Range(0f,1f)]
+	public float volume = 0.5f;
+	[Range(.1f,3f)]
+	public float pitch = 1f;
+
+	public bool Loop = false;
+
+	[HideInInspector]
+	public AudioSource source;
+}
